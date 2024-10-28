7a5d0c9f7cfb17e05412c90fbc9cf2f96ce5fae4
Paul Obinna <paulobinna493@gmail.com>
2024-10-28 12:23:02 +0100

    removed the class

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@ const container = document.querySelector(".container");
 
 open.addEventListener("click", () => {
   container.classList.add("show-nav");
-  document.body.classList.add("no-scroll");
 });
 close.addEventListener("click", () => {
   container.classList.remove("show-nav");
-  document.body.classList.remove("no-scroll");
 });
